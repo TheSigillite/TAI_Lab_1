e40e25f3110aeeb406b0875eca525a1f5f192e50
Michał Sitko <cprl.28342@gmail.com>
2020-03-13 16:39:32 +0100

    swaped navbar from hardcoded html to js script

---
+++ navbar.js
@@ -0,0 +1,13 @@
+let nav =  '<ul class="nav justify-content-center">'+
+    '<li class="nav-item">'+
+    '<a class="nav-link" href="index.html">Home</a>'+
+'</li>'+
+'<li class="nav-item">'+
+    '<a class="nav-link" href="quiz.html">Quiz</a>'+
+'</li>'+
+'<li class="nav-item">'+
+    '<a class="nav-link" href="contact.html" >Kontakt</a>'+
+'</li>'+
+'</ul>'
+let navbar = document.getElementById("navbar");
+navbar.innerHTML = nav;
